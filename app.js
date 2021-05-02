bad029b68bc8ec223a4b7cc495deff43557fd359
Ifrain David Medina Garcia <ifraindavidmedinagarcia@Ifrains-MacBook-Pro.local>
2021-05-01 20:13:15 -0400

    API form

---
+++ app.js
@@ -0,0 +1,59 @@
+
+const express = require('express')
+const cors = require('cors')
+const nodemailer = require('nodemailer')
+const PORT = 3001
+require('dotenv').config()
+const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors())
+
+
+app.post('/send', async (req, res) => {
+    const { name, lastName, email, message } = req.body
+
+    const contentHTML = `
+        <h1>User Information</h1>
+        <ul>
+            <li>${name}</li>
+            <li>${lastName}</li>
+            <li>${email}</li>
+        </ul>
+        <p>${message}</p>
+    `
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.user,
+            pass: process.env.pass
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    try {
+        const info = await transporter.sendMail({
+            from: 'My Portfolio',
+            to: 'medina.ifrain@gmail.com',
+            subject: "New message from your Portfolio",
+            html: contentHTML
+        })
+        if (info.messageId) {
+            res.status(200).json(info)
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started in PORT: ${PORT}`);
+})
+
+module.exports = app
